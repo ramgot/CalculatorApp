49d2196c1137c8d10b8a2d7452827daa49fe6b44
ramgot <RamazanovOY@mpei.ru>
2023-11-20 18:28:32 +0300

    homework

--- app/src/main/java/ru/turing/secondlesson_calculator/AuthorActivity.kt
+++ app/src/main/java/ru/turing/secondlesson_calculator/AuthorActivity.kt
@@ -1,11 +1,21 @@
 package ru.turing.secondlesson_calculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.turing.secondlesson_calculator.databinding.ActivityAuthorBinding
 
 class AuthorActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAuthorBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_author)
+        binding = ActivityAuthorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val textForPreviousResult: String
+
+        intent.getStringExtra(MainActivity.RESULT_EXTRA)?.let {
+            textForPreviousResult = "${getString(R.string.previous_activity_result)} $it"
+            binding.authorPreviousText.text = textForPreviousResult
+        }
     }
 }
